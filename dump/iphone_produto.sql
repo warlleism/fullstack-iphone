d66a58366753311cff14948a4698355c4b46ec7b
Warllei Martins <warllei.s.martins@hotmail.com>
2022-09-12 02:36:29 -0300

    correção

--- dump/iphone_produto.sql
+++ dump/iphone_produto.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `iphone` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `iphone`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: iphone
@@ -57,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-05 15:29:19
+-- Dump completed on 2022-09-12  2:34:25
